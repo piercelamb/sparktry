40dc04aee855f85d76c8996fad9f55b4c5941734
Pierce Lamb <richard.pierce.lamb@gmail.com>
2014-12-10 15:09:26 -0800

    lol

--- src/main/scala/sparktry.scala
+++ src/main/scala/sparktry.scala
@@ -1,7 +1,6 @@
 import java.io.{IOException, InputStreamReader, BufferedReader}
 import java.net.{MalformedURLException, URLConnection, URL}
 import java.text.SimpleDateFormat
-import java.util.Date
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
@@ -56,6 +55,7 @@ object sparktry {
     }
   }
 
+  val dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z")
 
   def main(args: Array[String]) {
     val logFile = "/home/plamb/Coding/Web_Analytics_POC/logtest/logtest.data" // Should be some file on your system
@@ -69,12 +69,29 @@ object sparktry {
       .flatMap(_.split("\n"))
       .map(parseLogEvent)
 
-    val timeStamps = logEvents.map(event => event.timestamp)
+
+// convert the parsed log into ip address, start time, start time, page requested
+    val ipTimeStamp = logEvents.map[(String, (Long, Long, String))](event => {
+      val time = dateFormat.parse(event.timestamp).getTime()
+
+      (event.ip, (time, time, event.requestPage))
+    })
+
+    // reduce the ipTimeStamp using IP's as keys and taking the max of the 2nd time (to find end time)
+    val latestSessionInfo = ipTimeStamp.
+      map[(String, (Long, Long, Long))](a => {
+          (a._1, (a._2._1, a._2._2, 1))
+    }).
+      reduceByKey((a, b) => {
+      (Math.min(a._1, b._1), Math.max(a._2, b._2), a._3 + b._3)
+    })//.
+     // updateStateByKey(updateStatbyOfSessions)
+
+  //  def updateStatebyOfSessions
     //val ipCounts = logEvents.map(event => (event.ip, 1)).reduceByKey(_ + _)
 
-
     //ipCounts.foreach(println)
-      timeStamps.foreach(println)
+      latestSessionInfo.foreach(println)
 
   }
 }
